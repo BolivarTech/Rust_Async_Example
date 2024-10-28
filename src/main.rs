7e5fb2c9884616fee0713232e183c2aba448b386
Julian Bolivar <julian.bolivar@emerson.com>
2024-10-28 16:17:09 -0400

    Function Documentation included

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,24 @@ use std::time::Duration;
 use rand::Rng;
 use tokio::time::sleep;
 
+/// The entry point of the application. This is an asynchronous main 
+/// function that spawns and runs multiple asynchronous tasks concurrently.
 #[tokio::main]
 async fn main()
 {
     let f = tokio::spawn(async_fn());
     let g = tokio::spawn(async_fn2());
     let h = tokio::spawn(long_cycle());
-    /*
-    let h = tokio::spawn( async move {
-    let _ = tokio::task::spawn_blocking(|| { long_cycle() }).await;
-    });
-    */
     println!("From Main!!!");
     h.await.unwrap();
     f.await.unwrap();
     g.await.unwrap();
 }
 
+/// An example asynchronous function that simulates reading from a database.
+///
+/// This function prints a message, reads a value from the database asynchronously, and
+/// then prints the result.
 async fn async_fn()
 {
     println!("Hello, async_fn!");
@@ -27,6 +28,10 @@ async fn async_fn()
     println!("First Result = {}", s1);
 }
 
+/// Another example asynchronous function that simulates reading from a database.
+///
+/// This function prints a message, reads a value from the database asynchronously, and
+/// then prints the result.
 async fn async_fn2()
 {
     println!("Hello, async_fn2!");
@@ -34,6 +39,18 @@ async fn async_fn2()
     println!("Second Result = {}", s2);
 }
 
+/// Simulates reading data from a database with random delay.
+///
+/// This function generates a random number of seconds to delay, awaits for that duration,
+/// and then returns a formatted string indicating the source of the read.
+///
+/// # Arguments
+///
+/// * `a` - A string slice that holds the name of the source.
+///
+/// # Returns
+///
+/// * `String` - A formatted string indicating which source was read.
 async fn read_from_db(a: &str) -> String
 {
     let num = rand::thread_rng().gen_range(0..10);
@@ -41,22 +58,27 @@ async fn read_from_db(a: &str) -> String
     format!("DB Read from {a}").to_owned()
 }
 
+/// A structure that holds the result of prime number verification.
 struct PrimeVerification {
     value: u32,
     is_prime: bool
 }
 
+/// A long-running asynchronous function that calculates prime numbers.
+///
+/// This function spawns blocking tasks to determine if numbers are prime, collects
+/// the prime numbers into a vector, and prints them.
 async fn long_cycle()
 {
-    let find_range: u32 = 100;
-    let num_taks = 8;
+    let find_range: u32 = 10_000;
+    let num_task = 8;
     let mut primes: Vec<u32> = Vec::new();
     let mut handles = Vec::new();
     let mut value: u32 = 0;
 
     println!("Long Calc Started!");
     loop {
-        while (handles.len() <= num_taks) && (value <= find_range)
+        while (handles.len() <= num_task) && (value <= find_range)
         {
             let result = tokio::task::spawn_blocking(move || { is_prime(value) });
             handles.push(result);
@@ -78,7 +100,7 @@ async fn long_cycle()
                     }
                 }
             }
-            if handles.len() < num_taks
+            if handles.len() < num_task
             {
                 break;
             }
@@ -97,6 +119,18 @@ async fn long_cycle()
     println!("Long Calc Finished!");
 }
 
+/// Determines if a number is prime.
+///
+/// This function checks if the given number is prime by testing for divisibility
+/// from 2 to the square root of the number.
+///
+/// # Arguments
+///
+/// * `n` - The number to be checked.
+///
+/// # Returns
+///
+/// * `PrimeVerification` - A struct containing the value and its primality.
 fn is_prime(n: u32) -> PrimeVerification {
     let mut respond = PrimeVerification {
         value: n,
@@ -119,6 +153,7 @@ mod tests
 {
     use super::*;
 
+    /// Tests the prime number detection logic.
     #[test]
     fn prime_number_test()
     {
